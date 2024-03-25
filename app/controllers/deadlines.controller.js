b75f54b7ab8c0c00fc89594f2b03f108283c4117
Avila George <george.avila@external.bper.it>
2024-03-25 17:10:32 +0100

    deadlines controller promisses

--- app/controllers/deadlines.controller.js
+++ app/controllers/deadlines.controller.js
@@ -8,41 +8,24 @@ var moment = require("moment/moment");
 
 exports.allDeadlines = (req, res) => {
   const idCompany = req.body.idCompany;
-  const startOfMonth = moment()
-    .set({ year: req.body.year, month: req.body.month })
-    .startOf("month")
-    .format("YYYY-MM-DD 00:00");
-  const endOfMonth = moment()
-    .set({ year: req.body.year, month: req.body.month })
-    .endOf("month")
-    .format("YYYY-MM-DD 23:59");
-  if (idCompany > 0) {
-    Entity.findAll({
-      include: [
-        {
-          model: Deadlines,
-          as: "deadlines",
-          where: {
-            companyId: idCompany,
-            expireDate: {
-              [Op.between]: [startOfMonth, endOfMonth],
-            },
-          },
-        },
-        {
-          model: Company,
-          as: "company",
-        },
-      ],
-    })
-      .then((data) => {
-        res.status(200).send(data);
-      })
-      .catch((err) => {
-        res.status(500).send({ message: err.message });
-      });
-  } else {
-    Entity.findAll({
+  const months = req.body.months; // Array di mesi valori numerici
+  const year = req.body.year;
+
+  // Array per memorizzare le promesse di ricerca delle scadenze per ogni mese
+  const promises = [];
+
+  // Per ogni mese nell'array dei mesi, creiamo una promessa per trovare le scadenze
+  months.forEach((month) => {
+    const startOfMonth = moment()
+      .set({ year: year, month: month })
+      .startOf("month")
+      .format("YYYY-MM-DD 00:00");
+    const endOfMonth = moment()
+      .set({ year: year, month: month })
+      .endOf("month")
+      .format("YYYY-MM-DD 23:59");
+
+    let queryOptions = {
       include: [
         {
           model: Deadlines,
@@ -58,12 +41,24 @@ exports.allDeadlines = (req, res) => {
           as: "company",
         },
       ],
+    };
+
+    if (idCompany > 0) {
+      queryOptions.include[0].where.companyId = idCompany;
+    }
+
+    // Esegui la query per trovare le entità con le scadenze per questo mese
+    promises.push(Entity.findAll(queryOptions));
+  });
+
+  // Aspetta che tutte le promesse siano risolte e restituisci i dati quando tutte sono state completate
+  Promise.all(promises)
+    .then((data) => {
+      // Concatena i risultati di ciascuna ricerca dei mesi
+      const allEntities = data.reduce((acc, val) => acc.concat(val), []);
+      res.status(200).send(allEntities);
     })
-      .then((data) => {
-        res.status(200).send(data);
-      })
-      .catch((err) => {
-        res.status(500).send({ message: err.message });
-      });
-  }
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
 };
