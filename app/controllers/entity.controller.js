37b2894eb84924ff8a19c985f2daf40301910471
Avila George <george.avila@external.bper.it>
2024-04-03 16:36:10 +0200

    add entities

---
+++ app/controllers/entity.controller.js
@@ -0,0 +1,31 @@
+const db = require("../models");
+const Entity = db.entity;
+
+
+exports.allEntities= (req, res) => {
+  const idCompany = req.body.idCompany;
+  if (idCompany > 0) {
+    Entity.findAll({
+      where: {
+        companyId: idCompany,
+      },
+      order: [["createdAt", "DESC"]],
+    })
+      .then((entities) => {
+        res.status(200).send(entities);
+      })
+      .catch((err) => {
+        res.status(500).send({ message: err.message });
+      });
+  } else {
+    Attendance.findAll({
+      order: [["createdAt", "DESC"]],
+    })
+      .then((entities) => {
+        res.status(200).send(entities);
+      })
+      .catch((err) => {
+        res.status(500).send({ message: err.message });
+      });
+  }
+};
