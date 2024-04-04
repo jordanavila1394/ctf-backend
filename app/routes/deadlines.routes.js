6b88de1a3f9104a75e381b36dc303c7203468a79
Avila George <george.avila@external.bper.it>
2024-04-04 09:58:43 +0200

    files

--- app/routes/deadlines.routes.js
+++ app/routes/deadlines.routes.js
@@ -4,9 +4,6 @@ const controller = require("../controllers/deadlines.controller");
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "/");
-  },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   },
