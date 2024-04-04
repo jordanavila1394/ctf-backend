8ac318877bae0d2dd8ce2cb6a314fa280713c0e0
Avila George <george.avila@external.bper.it>
2024-04-04 09:42:24 +0200

    try fix

--- app/routes/deadlines.routes.js
+++ app/routes/deadlines.routes.js
@@ -3,7 +3,14 @@ const controller = require("../controllers/deadlines.controller");
 
 const multer = require("multer");
 
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 const upload = multer({ storage: storage });
 
 module.exports = function (app) {
